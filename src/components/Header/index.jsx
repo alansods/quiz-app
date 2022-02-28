2267941e54100a85cf7c1e5469a6ecd2183fa48e
Alan Santos <alansods@gmail.com>
2022-02-28 18:58:06 -0300

    fix user

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,6 +10,7 @@ export default function Header({userName, step}) {
       <div>
         {step === 1 && <div className={style.container}><FaUserAlt />Usuário</div>}
         {step === 2 && <div className={style.container}><FaUserAlt />{userName}</div>}
+        {step === 3 && <div className={style.container}><FaUserAlt />{userName}</div>}
         {step === 4 && <div className={style.container}><FaUserAlt />{userName}</div>}
       </div>
     </header>
